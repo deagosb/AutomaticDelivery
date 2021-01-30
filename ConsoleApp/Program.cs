e5921f1b58c3ca762885995e2e2987f4367601e5
Diego Barrero <jbarred9@onjci.com>
2021-01-29 21:20:05 -0500

    Update ConsoleApp

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,12 +1,107 @@
-﻿using System;
+﻿using Domain;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Text;
 
 namespace ConsoleApp
 {
-    class Program
+    /// <summary>
+    /// Defines the <see cref="Program" />.
+    /// </summary>
+    internal class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// The Main.
+        /// </summary>
+        /// <param name="args">The args<see cref="string[]"/>.</param>
+        internal static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            if (Directory.Exists(Global.Path + @"\inputs\"))
+            {
+
+                // Get file in the path 
+                string[] fileEntries = Directory.GetFiles(Global.Path + @"\inputs\");
+
+                // Clean the output directory 
+                CleanDirectory(Global.Path + @"\outputs\");
+
+                // Process the list of files found in the directory.
+                foreach (string filePath in fileEntries)
+                {
+                    if (filePath.Contains(".txt") && filePath.Contains("in"))
+                    {
+                        // Get substring with the file number 
+                        string fileNumber = filePath.Substring(filePath.Length - 6, 2);
+
+                        // Process the file
+                        ProcessFile(filePath, fileNumber);
+                    }
+
+                }
+            }
+        }
+
+        /// <summary>
+        /// The ProcessFile.
+        /// </summary>
+        /// <param name="filePath">The filePath<see cref="string"/>.</param>
+        /// <param name="fileNumber">The fileNumber<see cref="string"/>.</param>
+        public static void ProcessFile(string filePath, string fileNumber)
+        {
+            Console.WriteLine("Processed file '{0}'.", filePath);
+
+            int counter = 0;
+            string line;
+
+            // Read the file and display it line by line.  
+            StreamReader file = new StreamReader(filePath);
+            while ((line = file.ReadLine()) != null && counter < Global.Maximum_Number_Of_Lunches)
+            {
+
+                Dron dron = new Dron(new Point(0, 0), Orientation.North);
+                dron.Run(line);
+                Console.WriteLine("({0}, {1}) direction {2}", dron.Point.X, dron.Point.Y, dron.Orientation);
+
+
+                // Set the output fileName  
+                StringBuilder sbOutputFileName = new StringBuilder();
+                sbOutputFileName.Append(Global.Path);
+                sbOutputFileName.Append(@"\outputs\out");
+                sbOutputFileName.Append(fileNumber);
+                sbOutputFileName.Append(".txt");
+
+
+                using (StreamWriter writer = new StreamWriter(sbOutputFileName.ToString(), true))
+                {
+                    writer.WriteLine("({0}, {1}) direction {2}", dron.Point.X, dron.Point.Y, dron.Orientation);
+                }
+
+                counter++;
+            }
+
+            file.Close();
+
+            Console.WriteLine("There were {0} lines.", counter);
+        }
+
+        /// <summary>
+        /// The CleanDirectory.
+        /// </summary>
+        /// <param name="path">The path<see cref="string"/>.</param>
+        private static void CleanDirectory(string path)
+        {
+            DirectoryInfo di = new DirectoryInfo(path);
+
+            foreach (FileInfo file in di.GetFiles())
+            {
+                file.Delete();
+            }
+            foreach (DirectoryInfo dir in di.GetDirectories())
+            {
+                dir.Delete(true);
+            }
         }
     }
 }
---
+++ Domain/Global.cs
@@ -0,0 +1,18 @@
+﻿namespace Domain
+{
+    /// <summary>
+    /// Defines the <see cref="Global" />.
+    /// </summary>
+    public static class Global
+    {
+        /// <summary>
+        /// Defines the Maximum_Number_Of_Lunches.
+        /// </summary>
+        public static int Maximum_Number_Of_Lunches = 3;
+
+        /// <summary>
+        /// Defines the path.
+        /// </summary>
+        public static string Path = @"C:\temp\";
+    }
+}
